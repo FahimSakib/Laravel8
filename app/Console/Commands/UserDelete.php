110b2e83b940b4f4927438adbe8570f263cc5a41
FahimSakib <fsakib991@gmail.com>
2022-02-24 10:17:06 +0600

    creating own commend and task scheduling

---
+++ app/Console/Commands/UserDelete.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UserDelete extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete the users whose emails are not verified';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::where('email_verified_at',null)->delete();
+        
+        $this->info('Unverified users deleted successfully');
+    }
+}
